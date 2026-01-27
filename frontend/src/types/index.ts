5fc05e21a1f24f151eafc2697be8c2518dba01e5
Tanapat <98180053+TeEnInDy@users.noreply.github.com>
2026-01-28 03:29:58 +0700

    feat: initial monorepo with integrated frontend and backend

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,10 @@
+export interface Order {
+    id: string;
+    seller_name: string;
+    customer_name: string;
+    products: string;
+    total_price: number;
+    status: string;
+    note: string;
+    created_at: string;
+  }
